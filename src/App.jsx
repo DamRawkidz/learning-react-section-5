d38eb717aed12541a84424257a7187198a94fafc
Phet <rawkidz@hotmail.com>
2024-07-25 01:43:09 +0700

    [update] - finish couse section 54

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import Header from "./components/Header.jsx"
+import Results from "./components/Result.jsx"
 import UserInput from "./components/UserInput.jsx"
+import { useState } from "react"
 function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  })
+
+  function handleChange(inputIndentifier, newValue) {
+    setUserInput(pervUserInput => {
+      return {
+        ...pervUserInput,
+        [inputIndentifier]: +newValue
+      }
+    })
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
     </>
   )
 }
